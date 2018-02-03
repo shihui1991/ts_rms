1640d7137696ae6747ed1d865d0722c684203b17
bulian <452853270@qq.com>
2018-02-03 16:22:30 +0800

    罗——模型调整

--- app/Http/Model/Dept.php
+++ app/Http/Model/Dept.php
@@ -13,7 +13,7 @@ class Dept extends Model
     use SoftDeletes;
     protected $table='dept';
     protected $primaryKey='id';
-    protected $guarded=['id','parent_id'];
+    protected $fillable=['name','type','infos'];
     protected $dates=['created_at','updated_at','deleted_at'];
     protected $casts = [];
     /* ++++++++++ 数据字段注释 ++++++++++ */
--- app/Http/Model/Menu.php
+++ app/Http/Model/Menu.php
@@ -13,7 +13,7 @@ class Menu extends Model
     use SoftDeletes;
     protected $table='a_menu';
     protected $primaryKey='id';
-    protected $guarded=['id','parent_id'];
+    protected $fillable=['name','icon','module','url','method','login','auth','display','sort','infos'];
     protected $dates=['created_at','updated_at','deleted_at'];
     protected $casts = [];
     /* ++++++++++ 数据字段注释 ++++++++++ */
