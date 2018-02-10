3e1ee2ac35e6b8c4fec9be82dc2e236557009c45
bulian <452853270@qq.com>
2018-02-10 09:49:48 +0800

    罗——后台-状态代码

--- resources/views/system/statecode/index.blade.php
+++ resources/views/system/statecode/index.blade.php
@@ -9,32 +9,47 @@
     </div>
 
     @if(filled($sdata))
-        <table class="table table-hover table-bordered">
-            <thead>
-            <tr>
-                <th>代码</th>
-                <th>名称</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
+        <div class="row">
+            <div class="col-xs-12">
+                <table class="table table-hover table-bordered">
+                    <thead>
+                    <tr>
+                        <th>代码</th>
+                        <th>名称</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
 
-            @foreach($sdata as $statecode)
-                <tr>
-                    <td>{{$statecode->code}}</td>
-                    <td>{{$statecode->name}}</td>
-                    <td>
-                        <div class="btn-group">
-                            <a href="{{route('sys_statecode_edit',['id'=>$statecode->id])}}" class="btn btn-xs">
-                                修改
-                            </a>
-                        </div>
-                    </td>
-                </tr>
+                    @foreach($sdata as $statecode)
+                        <tr>
+                            <td>{{$statecode->code}}</td>
+                            <td>{{$statecode->name}}</td>
+                            <td>
+                                <div class="btn-group">
+                                    <a href="{{route('sys_statecode_edit',['id'=>$statecode->id])}}" class="btn btn-xs">
+                                        修改
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
 
-            @endforeach
-            </tbody>
-        </table>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-6">
+                <div class="dataTables_info" id="dynamic-table_info" role="status" aria-live="polite">共 {{$sdata->total()}} 条数据</div>
+            </div>
+            <div class="col-xs-6">
+                <div class="dataTables_paginate paging_simple_numbers" id="dynamic-table_paginate">
+                    {{ $sdata->links() }}
+                </div>
+            </div>
+        </div>
     @endif
 
 @endsection
