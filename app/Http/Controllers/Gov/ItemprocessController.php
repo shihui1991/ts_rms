75111c1302e1335093fb6172dae8922dbb5db94c
bulian <452853270@qq.com>
2018-02-28 18:09:11 +0800

    罗——征收-项目审查-项目启动

--- app/Http/Controllers/Gov/ItemprocessController.php
+++ app/Http/Controllers/Gov/ItemprocessController.php
@@ -2456,6 +2456,161 @@ class ItemprocessController extends BaseitemController
 
     /* ========== 项目审查 - 项目启动 ========== */
     public function check_item_start(Request $request){
+        if($request->isMethod('get')){
+            /* ********** 查询 ********** */
+            DB::beginTransaction();
+            try{
+                $item=$this->item;
+                if(blank($item)){
+                    throw new \Exception('项目不存在',404404);
+                }
+                /* ++++++++++ 检查项目状态 ++++++++++ */
+                if($item->schedule_id!=1 || $item->process_id!=12 ||  $item->code!='22'){
+                    throw new \Exception('当前项目处于【'.$item->schedule->name.' - '.$item->process->name.'('.$item->state->name.')】，不能进行当前操作',404404);
+                }
 
+                $result=$this->hasNotice();
+                $process=$result['process'];
+                $worknotice=$result['worknotice'];
+
+                $worknotices=Worknotice::with(['process'=>function($query){
+                    $query->select(['id','name','type']);
+                },'dept'=>function($query){
+                    $query->select(['id','name']);
+                },'role'=>function($query){
+                    $query->select(['id','name']);
+                },'user'=>function($query){
+                    $query->select(['id','name']);
+                },'state'=>function($query){
+                    $query->select(['code','name']);
+                }])
+                    ->where('item_id',$this->item_id)
+                    ->where('schedule_id',$process->schedule_id)
+                    ->whereNotIn('code',['0','20'])
+                    ->orderBy('updated_at','desc')
+                    ->orderBy('id','desc')
+                    ->sharedLock()
+                    ->get();
+
+                $code='success';
+                $msg='查询成功';
+                $sdata=['item'=>$item,'worknotices'=>$worknotices];
+                $edata=null;
+                $url=null;
+
+                $view='gov.itemprocess.check.item_start';
+            }catch (\Exception $exception){
+                $code='error';
+                $msg=$exception->getCode()==404404?$exception->getMessage():'网络异常';
+                $sdata=null;
+                $edata=null;
+                $url=null;
+
+                $view='gov.error';
+            }
+            DB::commit();
+
+            /* ++++++++++ 结果 ++++++++++ */
+            $result=['code'=>$code,'message'=>$msg,'sdata'=>$sdata,'edata'=>$edata,'url'=>$url];
+            if($request->ajax()){
+                return response()->json($result);
+            }else{
+                return view($view)->with($result);
+            }
+        }
+        /* ********** 审查结果 ********** */
+        else{
+            DB::beginTransaction();
+            try{
+                $item=$this->item;
+                if(blank($item)){
+                    throw new \Exception('项目不存在',404404);
+                }
+                /* ++++++++++ 检查项目状态 ++++++++++ */
+                if($item->schedule_id!=1 || $item->process_id!=12 ||  $item->code!='22'){
+                    throw new \Exception('当前项目处于【'.$item->schedule->name.' - '.$item->process->name.'('.$item->state->name.')】，不能进行当前操作',404404);
+                }
+
+                $result=$this->hasNotice();
+                $process=$result['process'];
+                $worknotice=$result['worknotice'];
+
+                $worknotice->code='2';
+                $worknotice->save();
+                if(blank($worknotice)){
+                    throw new \Exception('操作失败',404404);
+                }
+
+                /* ++++++++++ 删除相同工作推送 ++++++++++ */
+                Worknotice::lockForUpdate()
+                    ->where([
+                        ['item_id',$item->id],
+                        ['schedule_id',$process->schedule_id],
+                        ['process_id',$process->id],
+                        ['menu_id',$process->menu_id],
+                        ['code','0'],
+                    ])
+                    ->delete();
+
+                /* ++++++++++ 项目初步预算 可操作人员 ++++++++++ */
+                $process=Process::with(['processusers'=>function($query){
+                    $query->with('role');
+                }])
+                    ->select(['id','schedule_id','menu_id'])
+                    ->find(14);
+                $values=[];
+                /* ++++++++++ 项目初步预算 工作提醒推送 ++++++++++ */
+                foreach ($process->processusers as $user){
+                    $values[]=[
+                        'item_id'=>$item->id,
+                        'schedule_id'=>$process->schedule_id,
+                        'process_id'=>$process->id,
+                        'menu_id'=>$process->menu_id,
+                        'dept_id'=>$user->dept_id,
+                        'parent_id'=>$user->role->parent_id,
+                        'role_id'=>$user->role_id,
+                        'user_id'=>$user->id,
+                        'url'=>route('g_itemprocess_csc',['item'=>$this->item->id]),
+                        'code'=>'20',
+                        'created_at'=>date('Y-m-d H:i:s'),
+                        'updated_at'=>date('Y-m-d H:i:s'),
+                    ];
+                }
+
+                $field=['item_id','schedule_id','process_id','menu_id','dept_id','parent_id','role_id','user_id','url','code','created_at','updated_at'];
+                $sqls=batch_update_or_insert_sql('item_work_notice',$field,$values,'updated_at');
+                if(!$sqls){
+                    throw new \Exception('操作失败',404404);
+                }
+                foreach ($sqls as $sql){
+                    DB::statement($sql);
+                }
+
+                $item->schedule_id=$worknotice->schedule_id;
+                $item->process_id=$worknotice->process_id;
+                $item->code=$worknotice->code;
+                $item->save();
+
+                $code='success';
+                $msg='操作成功';
+                $sdata=null;
+                $edata=null;
+                $url=route('g_itemprocess',['item'=>$this->item->id]);
+
+                DB::commit();
+            }catch (\Exception $exception){
+                $code='error';
+                $msg=$exception->getCode()==404404?$exception->getMessage():'网络异常';
+                $sdata=null;
+                $edata=null;
+                $url=null;
+
+                DB::rollBack();
+            }
+
+            /* ++++++++++ 结果 ++++++++++ */
+            $result=['code'=>$code,'message'=>$msg,'sdata'=>$sdata,'edata'=>$edata,'url'=>$url];
+            return response()->json($result);
+        }
     }
 }
---
+++ resources/views/gov/itemprocess/check/item_start.blade.php
@@ -0,0 +1,87 @@
+{{-- 继承布局 --}}
+@extends('gov.main')
+
+
+{{-- 页面内容 --}}
+@section('content')
+
+    <p>
+        <a class="btn btn-danger" onclick="btnAct(this)" data-url="{{route('g_itemprocess_cis',['item'=>$sdata['item']->id])}}" data-method="post">
+            <i class="ace-icon fa fa-check-circle bigger-110"></i>
+            项目启动
+        </a>
+    </p>
+
+    <div class="widget-box widget-color-grey collapsed">
+        <div class="widget-header">
+            <h4 class="widget-title lighter smaller">工作日志</h4>
+            <div class="widget-toolbar">
+                <a href="#" data-action="collapse">
+                    <i class="ace-icon fa fa-chevron-down"></i>
+                    展开/关闭
+                </a>
+            </div>
+        </div>
+        <div class="widget-body">
+            <div class="widget-main padding-8">
+
+                <div class="timeline-container timeline-style2">
+
+                    <div class="timeline-items">
+
+                        @foreach($sdata['worknotices'] as $worknotice)
+
+                            <div class="timeline-item clearfix">
+                                <div class="timeline-info">
+                                    <span class="timeline-date">{{$worknotice->updated_at}}</span>
+
+                                    <i class="timeline-indicator btn btn-info no-hover"></i>
+                                </div>
+
+                                <div class="widget-box transparent">
+                                    <div class="widget-body">
+                                        <div class="widget-main no-padding">
+                                            <span class="bigger-110">{{$worknotice->process->name}} 【{{$worknotice->state->name}}】</span>
+                                            <br>
+                                            <i class="ace-icon fa fa-user green bigger-125"></i>
+                                            <span>{{$worknotice->dept->name}} - {{$worknotice->role->name}} - {{$worknotice->user->name}}</span>
+                                            <br>
+                                            <a href="{{route('g_infos_info',['id'=>$worknotice->id])}}" title="查看详情">
+                                                <i class="ace-icon fa fa-info-circle blue bigger-125"></i>
+                                                @if($worknotice->process->getOriginal('type')==2) 审查意见： @endif
+                                            </a>
+                                            <span>{{$worknotice->infos}}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        @endforeach
+
+                    </div><!-- /.timeline-items -->
+                </div><!-- /.timeline-container -->
+
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+{{-- 样式 --}}
+@section('css')
+    
+    <link rel="stylesheet" href="{{asset('viewer/viewer.min.css')}}" />
+
+@endsection
+
+{{-- 插件 --}}
+@section('js')
+    @parent
+    <script src="{{asset('viewer/viewer.min.js')}}"></script>
+    <script src="{{asset('js/func.js')}}"></script>
+    <script>
+        $('#name').focus();
+        $('.img-content').viewer();
+    </script>
+
+@endsection
