815e6a0ccd84c4befc35174d407eeaf245f46767
seek-concert <1105420247@qq.com>
2018-03-10 10:11:49 +0800

    张_征收管理端_项目模块分页调整

--- resources/views/gov/householdobject/index.blade.php
+++ resources/views/gov/householdobject/index.blade.php
@@ -36,12 +36,7 @@
     </table>
     <div class="row">
         <div class="col-xs-6">
-            <div class="dataTables_info" id="dynamic-table_info" role="status" aria-live="polite">共 @if($code=='success') {{ $sdata->total() }} @else 0 @endif 条数据</div>
-        </div>
-        <div class="col-xs-6">
-            <div class="dataTables_paginate paging_simple_numbers" id="dynamic-table_paginate">
-                @if($code=='success') {{ $sdata->links() }} @endif
-            </div>
+            <div class="dataTables_info" id="dynamic-table_info" role="status" aria-live="polite">共 @if($code=='success') {{ count($sdata) }} @else 0 @endif 条数据</div>
         </div>
     </div>
 
