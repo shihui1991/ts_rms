73dbbe4f95d76a93adbc016c3e24787914c42f07
seek-concert <1105420247@qq.com>
2018-03-22 14:43:21 +0800

    张_评估机构端_菜单

--- app/Http/Controllers/Com/CompanyuserController.php
+++ app/Http/Controllers/Com/CompanyuserController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
-class CompanyuserController extends BaseController
+class CompanyuserController extends BaseauthController
 {
     /* ++++++++++ 初始化 ++++++++++ */
     public function __construct()
