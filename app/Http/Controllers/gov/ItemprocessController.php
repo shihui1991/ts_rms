acc61ce98b83cbf65b86145b9f38014c67a22d87
bulian <452853270@qq.com>
2018-04-04 14:18:13 +0800

    罗——征收-项目图片（重新提交）

--- app/Http/Controllers/gov/ItemprocessController.php
+++ app/Http/Controllers/gov/ItemprocessController.php
@@ -820,19 +820,19 @@ class ItemprocessController extends BaseitemController
                     throw new \Exception($validator->errors()->first(),404404);
                 }
 
-                $file_table_id=Filetable::where('name','item')->sharedLock()->value('id');
-                $file_cates=Filecate::where('file_table_id',$file_table_id)->sharedLock()->get();
-                $rules=[];
-                $messages=[];
-                foreach ($file_cates as $file_cate){
-                    $name='picture.'.$file_cate->filename;
-                    $rules[$name]='required';
-                    $messages[$name.'.required']='必须上传【'.$file_cate->name.'】';
-                }
-                $validator = Validator::make($request->all(),$rules,$messages);
-                if($validator->fails()){
-                    throw new \Exception($validator->errors()->first(),404404);
-                }
+//                $file_table_id=Filetable::where('name','item')->sharedLock()->value('id');
+//                $file_cates=Filecate::where('file_table_id',$file_table_id)->sharedLock()->get();
+//                $rules=[];
+//                $messages=[];
+//                foreach ($file_cates as $file_cate){
+//                    $name='picture.'.$file_cate->filename;
+//                    $rules[$name]='required';
+//                    $messages[$name.'.required']='必须上传【'.$file_cate->name.'】';
+//                }
+//                $validator = Validator::make($request->all(),$rules,$messages);
+//                if($validator->fails()){
+//                    throw new \Exception($validator->errors()->first(),404404);
+//                }
 
                 $result=$this->hasNotice();
                 $process=$result['process'];
